18e0ddce5af5be0c20b4f6de4a437af964ed7598
Vladislav <v.r.ryazantsev@gmail.com>
2022-11-12 12:58:29 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,28 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void initialize(){
+        phoneBook = new PhoneBook();
+    }
+    @AfterEach
+    public void close(){
+        phoneBook = null;
+    }
+
+    @Test
+    public void add(){
+        PhoneBook phoneBook = new PhoneBook();
+        var b = phoneBook.add();
+        Assertions.assertInstanceOf(Integer.class, b);
+    }
+
+
 }
